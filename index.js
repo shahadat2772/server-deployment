7968541381d40b08756abb6f426226a3c0359c9e
shahadat2772 <shahadat2772@gmailcom>
2022-12-10 22:50:43 +0600

    updated

--- index.js
+++ index.js
@@ -5,13 +5,58 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.trnyuya.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 app.get("/", async (req, res) => {
   res.send("HELLO");
 });
 
-app.get("/products", async (req, res) => {
-  res.send("In the product api");
-});
+async function run() {
+  try {
+    // Connecting with mongodb
+    await client.connect();
+
+    // Collections
+    const userCollection = client
+      .db("annoor-business")
+      .collection("users-collection");
+
+    const productCollection = client
+      .db("annoor-business")
+      .collection("product-collection");
+
+    app.get("/products", async (req, res) => {
+      res.send("In the product api");
+    });
+
+    app.get("/get-product", async (req, res) => {
+      try {
+        // const category = req.headers?.category;
+        // const filter = { category: category };
+        const result = await productCollection.find({}).toArray();
+        res.status(200).send({
+          data: result,
+          success: true,
+          message: "Got products data.",
+        });
+      } catch (error) {
+        res.status(500).send({
+          message: "Internal server error.",
+          success: false,
+        });
+      }
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 const PORT = process.env.PORT || 5000;
 
